bd71f0dc52601e63af4f855188bd5828e9a89ebb
mtbishop <bishopmatthew76@gmail.com>
2021-05-03 12:17:43 -0500

    + table components with Table class

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Component } from 'react';
+import API from '../utils/API';
+import './TableCSS.css';
+import Rows from './Rows';
 
-function Table() {
+export default class Table extends Component {
+  state = {
+    employeeArr: [],
+    location: '',
+  };
+
+  APICallWasSuccessful() {
+    API.getEmployees().then(({ d }) => {
+      this.setState({ employeeArr: d.results });
+    });
+  }
+
+  render() {
     return (
-        <div>
-            <h1>tables here</h1>
-        </div>
-    )
+      <div className="container">
+        <tbody>
+          {this.state.employeeArr.map((emp, i) => (
+            <Rows
+              key={i}
+              firstName={emp.name.first}
+              lastName={emp.name.last}
+              email={emp.email}
+              phone={emp.phone}
+              location={emp.location.state}
+              image={emp.picture.small}
+            />
+          ))}
+        </tbody>
+      </div>
+    );
+  }
 }
-
-export default Table;
