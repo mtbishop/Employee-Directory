b6238ba945c8b04628e5c45efd80a9806ae98242
mtbishop <bishopmatthew76@gmail.com>
2021-05-03 11:14:30 -0500

    got layout for charts component

--- src/components/RowsEnColumns.js
+++ src/components/RowsEnColumns.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
-function RowsEnColumns() {
+function RowsEnColumns({
+  firstName,
+  lastName,
+  email,
+  phone,
+  location,
+  image
+}) {
+
   return (
     <div>
-      <h4>RowsEnColumns</h4>
+      <tr>
+        <td>
+          <img alt="" src={image} />
+        </td>
+        <td>
+          <h2>{firstName}</h2>
+        </td>
+        <td>
+          <h2>{lastName}</h2>
+        </td>
+        <td>
+          <h5>{email}</h5>
+          <h5>{phone}</h5>
+          <h5>{location}</h5>
+        </td>
+      </tr>
     </div>
   );
 }
