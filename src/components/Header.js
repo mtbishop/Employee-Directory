de21df1e74580036c26babbcb4be4c0bc4fc9737
mtbishop <bishopmatthew76@gmail.com>
2021-05-03 11:33:19 -0500

    installed bootstrap, set up navbar in header, +css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './HeaderCSS.css';
+
+
 
 function Header() {
   return (
     <div>
       <header>
-        <h1>Employee Directory</h1>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" id="homeLink" href="/">
+            Employee Directory
+          </a>
+          <a
+            className="navbar-brand" id="repLink"
+            href="https://github.com/mtbishop/Employee-Directory"
+          >
+            Repository
+          </a>
+        </nav>
       </header>
     </div>
   );
